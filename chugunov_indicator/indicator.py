1d7bf4ebb07fe037935a8f3241440ca34219fa32
SamG-01 <sam1glosser@gmail.com>
2024-09-23 04:37:23 -0400

    add interpolation functions

---
+++ chugunov_indicator/indicator.py
@@ -0,0 +1,38 @@
+from scipy.interpolate import RegularGridInterpolator
+
+__all__ = ["intercept_interpolator", "skip_screening"]
+
+def intercept_interpolator(
+        inputs: tuple, intercepts,
+        method: str = "cubic"
+    ) -> RegularGridInterpolator:
+    """
+    Creates an Interpolator for finding the y-intercept of the D-T border curve.
+    
+    Keyword arguments:
+        `inputs`: a 5D grid of points (abar, zbar, z2bar, z1, z2)
+        `intercepts`: the negative y-intercepts computed from the `inputs`
+    """
+
+    return RegularGridInterpolator(inputs, intercepts, method)
+
+# pylint: disable=dangerous-default-value
+def skip_screening(state, scn_fac, interpolator: RegularGridInterpolator) -> bool:
+    """
+    Predicts whether screening can be skipped for a given rate calculation.
+
+    Keyword arguments:
+        `state`: a `PlasmaState` object
+        `scn_fac`: a `ScreenFactors` object
+        `interpolator`: an interpolator from `intercept_interpolator`
+    """
+
+    abar, zbar, z2bar = state.abar, state.zbar, state.z2bar
+    z1, z2 = scn_fac.z1, scn_fac.z2
+
+    xi = [abar, zbar, z2bar, z1, z2]
+
+    c = interpolator(xi)
+
+    T, D = state.temp, state.dens
+    return D < 1/10**c * T**3
