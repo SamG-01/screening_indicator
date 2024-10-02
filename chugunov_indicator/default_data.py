5396d2fef656a9744213f371c31f0f51e026e17e
SamG-01 <sam1glosser@gmail.com>
2024-10-02 02:17:58 -0400

    update everything

--- chugunov_indicator/default_data.py
+++ chugunov_indicator/default_data.py
@@ -14,12 +14,12 @@ class DefaultScreeningData:
     """Stores default data for screening indicator functions."""
 
     arrays = {
-        "abar": np.linspace(1, 75, 20, dtype=np.float32),
-        "log_z2bar": np.linspace(0, 3, 20, dtype=np.float32),
-        "z1": np.linspace(1, 25, 20, dtype=np.float32),
-        "z2": np.linspace(1, 25, 20, dtype=np.float32),
-        "T": np.logspace(7, 9.35, 35, dtype=np.float32),
-        "D": np.logspace(-4, 8, 35, dtype=np.float32)
+        "abar": np.linspace(1, 75, 20),
+        "log_z2bar": np.linspace(0, 3, 20),
+        "z1": np.linspace(1, 25, 20),
+        "z2": np.linspace(1, 25, 20),
+        "log_T": np.linspace(7, 9.35, 35),
+        "log_D": np.linspace(-4, 8, 35)
     }
 
     _grids = np.meshgrid(*arrays.values(), sparse=True, indexing="ij")
@@ -29,7 +29,7 @@ class DefaultScreeningData:
         grids[k] = _grids[j]
 
     inputs = [*arrays.values()][:-2]
-
+    
     try:
         intercepts = np.load(intercept_file)
     except FileNotFoundError:
--- chugunov_indicator/fitting.py
+++ chugunov_indicator/fitting.py
@@ -1,84 +1,16 @@
 import numpy as np
-from scipy.optimize import curve_fit
 
 from .chugunov_2009 import chugunov_2009
 
-__all__ = ["border_func", "border_from_grid", "intercept_from_border", "intercept_from_vars"]
-
-def border_func(T_border: np.ndarray, c: float) -> np.ndarray:
-    """
-    Finds the y-values of the D-T border curve given its x-values.
-
-    Keyword arguments:
-        `T_border`: temperatures (x-values) along the border
-        `c`: negative of y-intercept on log-log graph
-
-    Returns:
-        `D_border`: densities (y-values) along the border
-    """
-
-    return 1/10**c * T_border**3
-
-def border_from_grid(
-        T: np.ndarray, D: np.ndarray, F: np.ndarray,
-        lower: float = 1.005, upper: float = 1.01
-    ) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Finds the x and y data of the D-T border curve given screening factors on a grid.
-    """
-    border = (lower <= F) & (F <= upper)
-    return np.where(border, T, 0), np.where(border, D, 0)
-
-def _intercept_from_border(
-        T_border: np.ndarray, D_border: np.ndarray, return_pcov: bool = False
-    ) -> np.float32:
-    """
-    Finds the negative y-intercept `c` of the D-T border curve given its x and y data.
-
-    Keyword arguments:
-        `T_border`: temperatures (x-values) along the border
-        `D_border`: densities (y-values) along the border
-        `return_pcov`: whether to return `pcov`
-
-    Returns:
-        `popt`: negative `y`-intercept of the border curve in log-log space
-        `pcov`: variance of `popt`
-    """
-
-    # pylint: disable=unbalanced-tuple-unpacking
-    (popt,), ((pcov,),) = curve_fit(
-        border_func, T_border, D_border,
-        p0 = (23,), bounds=(21, 32)
-    )
-    if return_pcov:
-        return popt, pcov
-    return np.float32(popt)
-
-@np.vectorize(signature="(n,n),(n,n)->()", otypes=[np.float32])
-def intercept_from_border(T_border: np.ndarray, D_border: np.ndarray) -> np.float32:
-    """
-    Finds the negative y-intercept `c` of the D-T border curve given its x and y data. Vectorized version of _paramters_from_border.
-
-    Keyword arguments:
-        `T_border`: temperatures (x-values) along the border
-        `D_border`: densities (y-values) along the border
-
-    Returns:
-        `c`: negative`y`-intercept of the border curve in log-log space
-    """
-
-    T_border, D_border = np.ravel(T_border), np.ravel(D_border)
-    T_border, D_border = T_border[T_border != 0], D_border[T_border != 0]
-
-    return _intercept_from_border(T_border, D_border)
+__all__ = ["intercept_from_vars"]
 
 def intercept_from_vars(
-        T: np.ndarray, D: np.ndarray,
+        log_T: np.ndarray, log_D: np.ndarray,
         abar: float, log_z2bar: float,
         z1: float, z2: float,
         zbar: float = 4, a1: int = 4, a2: int = 12,
         lower: float = 1.005, upper: float = 1.01
-    ) -> np.float32:
+    ) -> float:
     """
     Finds the negative y-intercept `c` of the D-T border curve given physical variables.
 
@@ -95,8 +27,10 @@ def intercept_from_vars(
         `c`: negative `y`-intercept of the border curve in log-log space
     """
 
-    z2bar = 10**log_z2bar
+    F = chugunov_2009(10**log_T, 10**log_D, abar, zbar, 10**log_z2bar, z1, z2, a1, a2)
 
-    F = chugunov_2009(T, D, abar, zbar, z2bar, z1, z2, a1, a2)
-    T_border, D_border = border_from_grid(T, D, F, lower, upper)
-    return intercept_from_border(T_border, D_border)
+    border = (lower <= F) & (F <= upper)
+    log_T_border = np.where(border, log_T, np.nan)
+    log_D_border = np.where(border, log_D, np.nan)
+
+    return np.nanmean(3*log_T_border - log_D_border, axis=(-1, -2))
