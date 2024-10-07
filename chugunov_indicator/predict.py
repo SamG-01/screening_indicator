af7d15381050f18755738c61c1dd2ba51c9eed0a
SamG-01 <sam1glosser@gmail.com>
2024-10-07 05:25:15 -0400

    add _skip_screening (vectorized)

--- chugunov_indicator/predict.py
+++ chugunov_indicator/predict.py
@@ -2,6 +2,22 @@ import numpy as np
 from numba import njit
 
 @njit()
+def _skip_screening(T, D, abar, z2bar, z1, z2) -> bool:
+    """
+    Predicts whether screening can be skipped for a given rate calculation.
+    """
+
+    C0, m1 = -10.132085464080653, 1.03
+    a2, k2 = 10.41502026, 0.02076143
+    a3, k3 = 7.73078747, 0.02600378
+
+    C = C0 + m1*np.log10(z2bar)
+    C += a2*(abar**(-k2) - abar**k2)
+    C += a3*(z1**k3 + z2**k3)**2
+
+    return np.log10(D) < 3 * np.log10(T) - C
+
+@njit()
 def skip_screening(plasma, scn_fac) -> bool:
     """
     Predicts whether screening can be skipped for a given rate calculation.
@@ -13,11 +29,5 @@ def skip_screening(plasma, scn_fac) -> bool:
 
     abar, z2bar = plasma.abar, plasma.z2bar
     z1, z2 = scn_fac.z1, scn_fac.z2
-    log_z2bar = np.log10(z2bar)
-
-    c = 20.1
-    c += log_z2bar + (1/abar**0.44 - 0.31 * abar**0.34)
-    c += 7.65*((z1**(0.0262) + z2**(0.0262))**2 - 4)
-
     T, D = plasma.temp, plasma.dens
-    return np.log10(D) < 3 * np.log10(T) - c
+    return _skip_screening(T, D, abar, z2bar, z1, z2)
