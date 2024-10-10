90410a9ef8fbf4e480c022c53468cea66547a6cc
SamG-01 <sam1glosser@gmail.com>
2024-10-10 01:21:24 -0400

    makes _screening_intercept give C, splits Cn terms

--- chugunov_indicator/predict.py
+++ chugunov_indicator/predict.py
@@ -1,26 +1,29 @@
 import numpy as np
 from numba import njit
 
+__all__ = ["skip_chugunov_2009"]
+
 @njit()
-def _skip_screening(T, D, abar, z2bar, z1, z2) -> bool:
+def _screening_intercept(abar: float, z2bar: float, z1: float, z2: float) -> float:
     """
-    Predicts whether screening can be skipped for a given rate calculation.
+    Predicts the height of the D-T line that decides when screening is important.
     """
 
-    C0, m1 = -10.132085464080653, 1.03
+    m1 = 1.03
     a2, k2 = 10.41502026, 0.02076143
     a3, k3 = 7.73078747, 0.02600378
 
-    C = C0 + m1*np.log10(z2bar)
-    C += a2*(abar**(-k2) - abar**k2)
-    C += a3*(z1**k3 + z2**k3)**2
+    C0 = -10.132085464080653
+    C1 = m1*np.log10(z2bar)
+    C2 = a2*(abar**(-k2) - abar**k2)
+    C3 = a3*(z1**k3 + z2**k3)**2
 
-    return np.log10(D) < 3 * np.log10(T) - C
+    return C0 + C1 + C2 + C3
 
 @njit()
-def skip_screening(plasma, scn_fac) -> bool:
+def skip_chugunov_2009(plasma, scn_fac) -> bool:
     """
-    Predicts whether screening can be skipped for a given rate calculation.
+    Predicts whether screening for `chugunov_2009` can be skipped for a given rate calculation.
 
     Keyword arguments:
         `state`: a `PlasmaState` object
@@ -30,4 +33,6 @@ def skip_screening(plasma, scn_fac) -> bool:
     abar, z2bar = plasma.abar, plasma.z2bar
     z1, z2 = scn_fac.z1, scn_fac.z2
     T, D = plasma.temp, plasma.dens
-    return _skip_screening(T, D, abar, z2bar, z1, z2)
+
+    C = _screening_intercept(abar, z2bar, z1, z2)
+    return np.log10(D) < 3 * np.log10(T) - C
